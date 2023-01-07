b2081a1d815ca89882f9eca7360c02bb59190b85
Joseph Lee <josephlee3004@gmail.com>
2023-01-07 11:11:22 -0500

    Finish 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (
+        !Number.isInteger(num1) || !Number.isInteger(num2) || 
+        num1 < 0 || num2 < 0
+    ) return 'ERROR';
+    let sum = 0;
+    let smaller, bigger;
+    if (num1 <= num2) {
+        smaller = num1;
+        bigger = num2;
+    } else {
+        smaller = num2;
+        bigger = num1;
+    }
+    for (let i = smaller; i <= bigger; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
