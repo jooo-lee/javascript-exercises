ba9fd2da8e4c177bf678c71d6c24ec94f1e2f87e
Joseph Lee <josephlee3004@gmail.com>
2023-05-03 11:31:04 -0400

    Finish 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((tmpSum, arrElem) => tmpSum + arrElem, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.length ? arr.reduce((tmpSum, arrElem) => tmpSum * arrElem) : 0;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return Math.pow(x, y);
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	if (x === 0) {
+    return 1;
+  }
+  return x * factorial(x - 1);
 };
 
 // Do not edit below this line
