2c553001fc7690ddabf5896a8a8c4f5133a03472
Joseph Lee <josephlee3004@gmail.com>
2023-05-04 14:58:25 -0400

    Finish 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    // let punctuation = /[\.,?!\s]/g;
+    // let newStr = str.replace(punctuation, "").toLowerCase();
+    // let revStr = newStr.split("").reverse().join("");
+    // return newStr === revStr;
 
+    let processedStr = str.toLowerCase().replace(/[^a-z]/g, "");
+    return processedStr.split("").reverse().join("") === processedStr;
+
+    // Convert string to lowercase
+    // Get rid of all punctuation and white space using RegEx
+    // Split into array
+    // Reverse array
+    // Join array 
+    // Compare strings
 };
 
 // Do not edit below this line
