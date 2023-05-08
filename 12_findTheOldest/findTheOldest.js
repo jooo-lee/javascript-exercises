e09352955c47382a7cf146e24215579776e3e231
Joseph Lee <josephlee3004@gmail.com>
2023-05-08 15:16:28 -0400

    Finish 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    return arr.reduce((oldestPerson, currPerson) => {
+        const oldestPersonAge = getAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath);
+        const currPersonAge = getAge(currPerson.yearOfBirth, currPerson.yearOfDeath);
+        return oldestPersonAge > currPersonAge ? oldestPerson : currPerson;
+    });
+};
 
+const getAge = function(yearOfBirth, yearOfDeath) {
+    if (!yearOfDeath) {
+        return new Date().getFullYear() - yearOfBirth;
+    }
+    return yearOfDeath - yearOfBirth;
 };
 
 // Do not edit below this line
